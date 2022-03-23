b3a51c22ad6a624d82668294f701e8ccef68bda7
Laby Damaro Camara <ldamaro98@gmail.com>
2022-03-23 04:09:26 +0100

    service annuaire

---
+++ mastere-config-jb/src/test/java/com/stardevcgroup/mastereconfigjb/MastereConfigJbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stardevcgroup.mastereconfigjb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastereConfigJbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
