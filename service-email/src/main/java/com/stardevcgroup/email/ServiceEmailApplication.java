52e646fdc44b81304830d22053494c6181869129
Laby Damaro Camara <ldamaro98@gmail.com>
2022-03-23 06:07:01 +0100

    service mail

--- service-email/src/main/java/com/stardevcgroup/email/ServiceEmailApplication.java
+++ service-email/src/main/java/com/stardevcgroup/email/ServiceEmailApplication.java
@@ -1,8 +1,11 @@
-package com.stardevcgroup.proxy;
+package com.stardevcgroup.email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceEmailApplication {
 
--- service-email/src/test/java/com/stardevcgroup/email/ServiceEmailApplicationTests.java
+++ service-email/src/test/java/com/stardevcgroup/email/ServiceEmailApplicationTests.java
@@ -1,4 +1,4 @@
-package com.stardevcgroup.proxy;
+package com.stardevcgroup.email;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
